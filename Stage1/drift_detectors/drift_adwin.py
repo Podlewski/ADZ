6ddcebe8676a8a5d1fb08082187540eee8537f66
Karol Podlewski <podlewski@hotmail.com>
2020-11-15 12:44:33 +0100

    feat: Removed fliping prediction bool in ADWIN algorithm

--- Stage1/drift_detectors/drift_adwin.py
+++ Stage1/drift_detectors/drift_adwin.py
@@ -10,3 +10,15 @@ class DriftADWIN(DriftDetector):
         self.model = ADWIN(delta)
         self.change_indexes = []
         self.warning_zones_indexes = []
+
+    def catch_concept_drift(self, predictions):
+        """
+        Funkcja wyłapująca Concept drift oraz strefy zagrożenia. Pobiera ona
+        tablicę predykcji w postaci zmiennych logicznych.
+        """
+        for i in range(len(predictions)):
+            self.model.add_element(int(predictions[i]))
+            if self.model.detected_change():
+                self.change_indexes.append(i)          
+            if self.model.detected_warning_zone():
+                self.warning_zones_indexes.append(i)  
